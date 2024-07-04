b0f9a7a9a43e5c718204864228a9e54554f53e82
adilamillath <adilamillath@gmail.com>
2024-07-04 06:29:29 +0000

    updated

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,58 +1,62 @@
 import React from 'react';
 import { useCart } from '../../context/CartContext';
 import '../../components/Cart/Cart.css';
-import Header from '../../components/Header/Header'
+import Header from '../../components/Header/Header';
 import emptyCartAnimation from '../../assets/emptyCart.json';
 import Lottie from 'react-lottie-player';
 import { Link } from 'react-router-dom';
+import { IoMdTrash } from "react-icons/io";
+
 
 const Cart = () => {
   const { cart, removeFromCart, incrementQuantity, decrementQuantity, getCartTotal } = useCart();
 
   return (
     <div>
-      <div>
-        <Header />
-      </div>
+      <Header />
       <div className="cart">
         <h2>Your Cart</h2>
         {cart.length === 0 ? (
-        <div style={{ textAlign: 'center' }}>
-          <p>Your cart is empty</p>
-          <Lottie loop  animationData={emptyCartAnimation}
-            play
-            style={{ width: 300, height: 300 }}
-          />
-        </div>
-      ) :(
+          <div style={{ textAlign: 'center' }}>
+            <p>Your cart is empty</p>
+            <Lottie
+              loop
+              animationData={emptyCartAnimation}
+              play
+              style={{ width: 300, height: 300 }}
+            />
+          </div>
+        ) : (
           <>
             <ul>
               {cart.map(item => (
                 <li key={item.id}>
                   <img src={item.image} alt={item.title} />
-                  <div>
-                    <h3>{item.title}</h3>
-                    <p>Rs.{item.price}</p>
-                    <p>Quantity: {item.quantity}</p>
+                  <div className='cart-items-details'>
+                    <div>
+                      <h3>{item.title}</h3>
+                    <p>Price: Rs.{item.price}</p>
+                    </div>
+                    
                     <div className="quantity-actions">
                       <button onClick={() => decrementQuantity(item.id)}>-</button>
+                      <span>{item.quantity}</span>
                       <button onClick={() => incrementQuantity(item.id)}>+</button>
                     </div>
-                    <button onClick={() => removeFromCart(item.id)}>Remove</button>
+                    <div>
+                    <button onClick={() => removeFromCart(item.id)}><IoMdTrash /></button>
+
+                    </div>
                   </div>
                 </li>
               ))}
             </ul>
-            <h3>Total Price: Rs.{getCartTotal().toFixed(2)}</h3>
+            <h3 className="total-price">Total Price: Rs.{getCartTotal().toFixed(2)}</h3>
           </>
-        )}<div>
-        <Link to="/"><p>Back to home</p></Link>
-        </div>
-
+        )}
+        <Link to="/" className="backtohome">Back to home</Link>
       </div>
-      
     </div>
-
   );
 };
 
